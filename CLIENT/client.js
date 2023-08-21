ae937f6ac5de346d55682adaf8aec7c260f1dacb
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2023-08-20 20:16:37 -0600

    Update client.js

--- CLIENT/client.js
+++ CLIENT/client.js
@@ -1,7 +1,3 @@
-// Jose Andres Hernandez Guerra 20053
-// Cliente para el servidor de chat en Node.js
-
-// Importar librerías
 const { client, xml } = require("@xmpp/client");
 const { v4: uuidv4 } = require('uuid');
 const debug = require("@xmpp/debug");
@@ -10,7 +6,6 @@ const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
 
-// Desactivar las comprobaciones de certificados TLS 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 class SimpleXMPPClient {
@@ -23,40 +18,7 @@ class SimpleXMPPClient {
         this.xmpp = null;
     }
 
-    displaySubMenu() {
-        console.log('\n¿Qué deseas hacer?');
-        console.log('[1] Mostrar todos los usuarios/contactos y su estado');
-        console.log('[2] Agregar un usuario a los contactos');
-        console.log('[3] Mostrar detalles de contacto de un usuario');
-        console.log('[4] Comunicación 1 a 1 con cualquier usuario/contacto');
-        console.log('[5] Participar en conversaciones grupales');
-        console.log('[6] Definir mensaje de presencia');
-        console.log('[7] Enviar/recibir notificaciones');
-        console.log('[8] Enviar/recibir archivos');
-        console.log('[9] Regresar al menú principal');
-
-        rl.question('\nTu elección: ', (answer) => {
-            switch (answer) {
-                case '1':
-                    // Llamar a la función para mostrar todos los usuarios
-                    break;
-                case '2':
-                    // Llamar a la función para agregar un usuario
-                    break;
-                // ... (Así sucesivamente para cada opción)
-                case '9':
-                    displayMenu();
-                    break;
-                default:
-                    console.log('\nOpción no válida. Inténtalo de nuevo.');
-                    this.displaySubMenu();
-                    break;
-            }
-        });
-    }
-
-    // Método para conectarse al servidor XMPP
-    async connect(username, password) {
+    async connectToServer(username, password) {
         this.username = username;
         this.password = password;
 
@@ -67,74 +29,64 @@ class SimpleXMPPClient {
             password: this.password,
         });
 
-        // Manejar errores de conexión
         this.xmpp.on('error', err => {
             console.error('Error:', err);
         });
 
-        // Manejar conexión exitosa
         this.xmpp.on('online', address => {
-            console.log('\n + Conectado exitosamente como:', address.toString());
+            console.log('\n + Connected successfully as:', address.toString());
         });
 
-        // Intentar conectarse
         try {
             await this.xmpp.start();
-            console.log('\n + Conexión iniciada...');
+            console.log('\n + Connection started...');
         } catch (err) {
-            console.error('\n - Error al establecer la conexión:', err);
+            console.error('\n - Error establishing connection:', err);
         }
     }
 
-    // Método para registrar un nuevo usuario
-    async register(username, password) {
-        return new Promise(async (resolve, reject) => {
-            if (this.xmpp) {
-                reject(new Error('Ya existe una conexión.'));
-                return;
-            }
+    async registerNewUser(username, password) {
+        if (this.xmpp) {
+            throw new Error('A connection already exists.');
+        }
 
-            this.username = username;
-            this.password = password;
+        this.username = username;
+        this.password = password;
 
-            console.log('\n + Registrando usuario...');
-            console.log(' > Nombre de usuario  :', this.username);
-            console.log(" > Contraseña         :", this.password);
-            console.log(" > Correo             :", (this.username + '@prueba_her20053.com'))
-
-            this.xmpp = client({
-                service: this.service,
-                domain: this.domain,
-                username: this.username,
-                password: this.password,
-            });
-
-            try {
-                await this.xmpp.start();
-            } catch (err) {
-                reject(new Error('Error al establecer la conexión.'));
-            }
-
-            const registerStanza = xml(
-                'iq',
-                { type: 'set', id: 'register' },
-                xml('query', { xmlns: 'jabber:iq:register' },
-                    xml('username', {}, username),
-                    xml('password', {}, password),
-                    xml('email', {}, (username + '@prueba_her20053.com')))
-            );
-
-            this.xmpp.send(registerStanza).then(() => {
-                resolve();
-            }).catch((err) => {
-                reject(new Error('Error al registrar el usuario.'));
-            });
+        console.log('\n + Registering user...');
+        this.xmpp = client({
+            service: this.service,
+            domain: this.domain,
+            username: this.username,
+            password: this.password,
         });
+
+        try {
+            await this.xmpp.start();
+        } catch (err) {
+            throw new Error('Error establishing connection.');
+        }
+
+        const registerStanza = xml(
+            'iq',
+            { type: 'set', id: 'register' },
+            xml('query', { xmlns: 'jabber:iq:register' },
+                xml('username', {}, username),
+                xml('password', {}, password),
+                xml('email', {}, (username + '@prueba_her20053.com')))
+        );
+
+        try {
+            await this.xmpp.send(registerStanza);
+        } catch (err) {
+            throw new Error('Error registering the user.');
+        }
     }
 
-
-
-
+    showUserOptions() {
+        console.log('\nWhat would you like to do?');
+        // ... Rest of the options ...
+    }
 }
 
 const rl = readline.createInterface({
@@ -142,69 +94,56 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const displayMenu = () => {
+const mainMenu = () => {
+    console.log('\nChoose an option:');
+    console.log('1. Create an account');
+    console.log('2. Log in');
+    console.log('3. Exit');
 
-
-    console.log('\nSeleccione una opción:');
-    console.log('1. Crear una cuenta');
-    console.log('2. Iniciar sesión');
-    console.log('3. Salir');
-
-    rl.question('\nTu elección: ', (answer) => {
-
+    rl.question('\nYour choice: ', (answer) => {
         switch (answer) {
-
             case '1':
-                rl.question('\nNombre de usuario (para registro): ', username => {
-                    rl.question('Contraseña (para registro): ', password => {
-                        const myClient = new SimpleXMPPClient();
-                        myClient.register(username, password)
+                rl.question('\nUsername (for registration): ', username => {
+                    rl.question('Password (for registration): ', password => {
+                        const xmppClient = new SimpleXMPPClient();
+                        xmppClient.registerNewUser(username, password)
                             .then(() => {
-                                console.log('Usuario registrado exitosamente.');
-                                displayMenu();
+                                console.log('User registered successfully.');
+                                mainMenu();
                             })
                             .catch(err => {
-                                console.error('Error al registrar:', err.message);
-                                displayMenu();
+                                console.error('Registration error:', err.message);
+                                mainMenu();
                             });
                     });
                 });
                 break;
-
-
-
             case '2':
-                rl.question('\nNombre de usuario: ', username => {
-                    rl.question('Contraseña: ', password => {
-                        const myClient = new SimpleXMPPClient();
-                        myClient.connect(username, password)
+                rl.question('\nUsername: ', username => {
+                    rl.question('Password: ', password => {
+                        const xmppClient = new SimpleXMPPClient();
+                        xmppClient.connectToServer(username, password)
                             .then(() => {
-                                myClient.displaySubMenu();
+                                xmppClient.showUserOptions();
                             })
                             .catch(err => {
-                                console.error('Error al iniciar sesión:', err.message);
-                                displayMenu();
+                                console.error('Login error:', err.message);
+                                mainMenu();
                             });
                         rl.close();
                     });
                 });
                 break;
-
-
-
             case '3':
-                console.log('\nAdiós!');
+                console.log('\nGoodbye!');
                 rl.close();
                 break;
             default:
-                console.log('\nOpción no válida. Inténtalo de nuevo.');
-                displayMenu();
+                console.log('\nInvalid option. Try again.');
+                mainMenu();
                 break;
-
-
-
         }
     });
 };
 
-displayMenu();
+mainMenu();
